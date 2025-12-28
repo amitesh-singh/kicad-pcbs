3ba3400bc6a017aaa1e66d16e4b0fc75009fa1e5
Amitesh Singh <singh.amitesh@gmail.com>
2025-12-28 22:45:42 +0530

    add more

--- projects/led/output/generate_mill.sh
+++ projects/led/output/generate_mill.sh
@@ -2,7 +2,9 @@
 
 docker run --rm -i -t -v ".:/data" ptodorov/pcb2gcode
 
-cat back.ncg > combined.ncg
-cat drill.ncg >> combined.ncg
-cat outline.ncg >> combined.ncg
+cat back.ngc > combined.ngc
 
+#cat drill.ngc >> combined.ngc
+# if you want to drill mounting holes
+cat milldrill.ngc >> combined.ngc
+cat outline.ngc >> combined.ngc
